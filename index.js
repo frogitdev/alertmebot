6abf1b3b32a890eb9b019b3b4f45c34cf48fb22d
FrogIT <kimhg0149@gmail.com>
2017-08-30 21:58:47 +0900

    Upgrade logging, Add name calling
    log4js로 로깅 업그레이드 + 이름을 부르면 응답함

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 var app = express();
+const log4js = require("log4js");
+var logger = log4js.getLogger();
+
 
 const settings = require('./settings.json')
 const token = settings.tokenset;
 
+logger.level = "debug";
+
 let bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const resp = match[1];
+bot.onText(/\/start/, (msg, match) => {
+    bot.sendMessage(msg.chat.id, "텔레그램 한국어 가상 비서, alertMe(얼러트미) 입니다. 개발 진행 중이므로 불안정할 수 있습니다.");
+});
 
-    bot.sendMessage(chatId, resp);
+bot.onText(/\/say (.+)/, (msg, match) => {
+    bot.sendMessage(msg.chat.id, match[1]);
+    logger.debug(`/say command from @${msg.from.username}`);
+});
+
+bot.onText(/alertme|alertMe|얼러트미/, (msg, match) => {
+    bot.sendMessage(msg.chat.id, `네, ${msg.from.first_name}님?`);
+    logger.debug(`/alertme command from @${msg.from.username}`);
 });
 
 bot.on('message', (msg) => {
-    console.log("@"+msg.from.username+" - "+msg.text);
+    logger.debug(`(${msg.chat.title}) @${msg.from.last_name} ${msg.from.first_name} - ${msg.text}`);
 });
